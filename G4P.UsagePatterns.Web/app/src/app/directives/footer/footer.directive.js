4307ee644876efbfee19874406c8c44fe6722e4b
gilnahmias <gil.nahmias@gmail.com>
2015-01-08 20:52:33 -0800

    added directives

---
+++ G4P.UsagePatterns.Web/app/src/app/directives/footer/footer.directive.js
@@ -0,0 +1,10 @@
+var G4P = G4P || {};
+G4P.Directives = G4P.Directives || {};
+
+G4P.Directives.Footer = function(){
+	return {
+		restrict: 'E',
+		templateUrl: '/app/directives/footer/footer.html',
+		replace: true
+	};
+};
